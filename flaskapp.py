ff3747377f77237ee2d090d04373dabbacf3a6dc
Vegan Lroy <vl357@drexel.edu>
2019-06-11 20:42:56 -0400

    20190611 Login Page

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,17 @@
-from flask import Flask
 from functions import *
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return landingPage()
+def loginPage():
+    return app.send_static_file('./login/login.html')
+
+@app.route('/loginConfirm', methods=['POST'])
+def login():
+    pass
+
+@app.route('/register')
+def registerPage():
+    return 'WIP'
 
 if __name__ == '__main__':
     app.run()
